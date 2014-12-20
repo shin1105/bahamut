50f8937a40635ffc9b53ad9154f2651529dbc279
shin1105 <huas1932@gmail.com>
2014-12-20 15:08:03 +0800

    跑酷(跳躍、鏡頭跟隨、摔落)

---
+++ Assets/js/player.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var leftoffset : float = 19.0f;
+var isJumping : boolean = false;
+var speed : float =5.0f;
+var jumpForce : float =30.0f;
+
+function Start () {
+
+}
+
+function Update () {
+	rigidbody2D.velocity =new Vector2(speed, rigidbody2D.velocity.y);
+	if(Input.GetButtonDown("Jump")&& isJumping == false) {
+		rigidbody2D.AddForce(new Vector2(0,jumpForce));
+	}
+}
+function OnTriggerEnter2D (other : Collider2D) {
+	if(other.tag == "Ground") {
+		isJumping = false;
+	}
+}
+
+function OnTriggerExit2D (other : Collider2D) {
+	if(other.tag == "Ground") {
+		isJumping = true;
+	}
+}
